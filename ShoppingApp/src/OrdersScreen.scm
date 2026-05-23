652dbf5a45cd93f480dc6e7fff7ffd3726de5dbc
cutecindy15 <padrigocindy15@gmail.com>
2026-05-23 21:24:47 +0800

    Add OrdersScreen

---
+++ ShoppingApp/src/OrdersScreen.scm
@@ -0,0 +1,46 @@
+#|
+$JSON
+{
+  "YaVersion": "208",
+  "componentType": "Form",
+  "name": "OrdersScreen",
+  "uuid": "11111",
+  "properties": {
+    "Uuid": "11111",
+    "Title": "My Orders",
+    "Scrollable": true
+  },
+  "components": [
+    {
+      "Name": "Label_OrdersTitle",
+      "ComponentType": "Label",
+      "Uuid": "comp301",
+      "properties": {
+        "Text": "Your Orders",
+        "FontSize": 24,
+        "FontBold": true
+      }
+    },
+    {
+      "Name": "ListView_Orders",
+      "ComponentType": "ListView",
+      "Uuid": "comp302",
+      "properties": {
+        "Width": -1,
+        "Height": 500,
+        "Elements": []
+      }
+    },
+    {
+      "Name": "Button_ViewDetails",
+      "ComponentType": "Button",
+      "Uuid": "comp303",
+      "properties": {
+        "Text": "View Details",
+        "Width": -1
+      }
+    }
+  ]
+}
+$ENDCOMPONENT
+#|
